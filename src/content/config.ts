3c7693e0d7b0790acb87ea539abb604c50b7d661
Harry Skerritt <harry2.skerritt@live.uwe.ac.uk>
2026-02-08 18:10:47 +0000

    added the projects page

---
+++ src/content/config.ts
@@ -0,0 +1,16 @@
+import { defineCollection, z } from 'astro:content';
+
+const projects = defineCollection({
+    type: 'content',
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        image: z.string(),
+        tags: z.array(z.string()),
+        link: z.string().url(),
+        github: z.string().url().optional(),
+        category: z.enum(['game', 'other']),
+    }),
+});
+
+export const collections = { projects };
---
+++ src/content/projects/other/test-project.md
@@ -0,0 +1,11 @@
+---
+title: "Test Project"
+description: "This is a test project which i can use to enusre that the project works within the website."
+image: "/projects/test.jpg"
+tags: ["C++", "API", "Library"]
+link: "https://harry-skerritt.itch.io/"
+github: "https://github.com/Harry-Skerritt"
+category: "other"
+---
+# Test Content
+If you can see this, the collection is working!
